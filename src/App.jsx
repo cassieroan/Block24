cf768011d28b9caf21dc2353e7f67b22634b51ac
Cassie Roan <cassandracroan@gmail.com>
2023-08-03 16:09:15 -0400

    Added CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import './App.css'
-import {puppyList} from './data.js'
+import './App.css';
+import { puppyList } from './data.js';
+import { useState } from 'react';
+
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log(puppyList)
+  function handleClick(puppy) {
+    setFeatPupId(puppy.id);
+    console.log("puppy id: ", puppy.id);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
   return (
-    <>
-      <div>
-
-      </div>
-    </>
-  )
+    <div className="App">
+      <h1 className="logo">Our Puppy Pals!</h1>
+      <h3 className="read-the-docs">Select one to learn their information:</h3>
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => handleClick(puppy)} key={puppy.id} className="puppy-card">
+            {puppy.id}. {puppy.name}
+          </p>
+        );
+      })}
+      {featuredPup && (
+        <div className="card">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li><b>Age:</b> {featuredPup.age}</li>
+            <li><b>Email:</b> {featuredPup.email}</li>
+            <li>
+              <b>Cuteness status:</b> {featuredPup.isCute ? "Absolutely Adorable" : "Ooof, not really :/"}
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
+
