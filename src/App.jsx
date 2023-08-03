80b746a13a73f9ccc3c8a7505c83714ac063c076
Cassie Roan <cassandracroan@gmail.com>
2023-08-03 14:59:30 -0400

    Added data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import {puppyList} from './data.js'
 function App() {
 
+  console.log(puppyList)
   return (
     <>
       <div>
